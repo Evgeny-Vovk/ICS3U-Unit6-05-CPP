2970b199ce7b9bd0ebc37d3d094fae2b6d0158ff
Evgeny Vovk <Evgeny.vovk@mths.ca>
2023-01-22 16:33:17 +0000

    done

---
+++ ICS3U-Unit6-05.cpp
@@ -0,0 +1,43 @@
+// Copyright(c) 2022 Evgeny Vovk All rights reserved.
+//
+// Created by : Evgeny Vovk
+// Created on : January 2023
+// ICS3U-Unit6-05.cpp File, learning functions with parameters in C++.
+
+#include <iostream>
+#include <list>
+
+int smallestNumber(std::list<int> grades) {
+    int mark;
+    int sum = 0;
+    int average = 0;
+    int length = 0;
+    for (int mark : grades) {
+        sum += mark;
+        length += 1;
+    }
+    average = sum / length;
+    return average;
+}
+
+int main() {
+    // creating variables
+    std::list<int> grades;
+    int mark = 0;
+    int average;
+
+    std::cout << "Please enter one mark at a time. Enter -1 to end.\n";
+    std::cout << "What is your mark? (as %): ";
+    std::cin >> mark;
+
+    while (mark != -1) {
+        grades.push_back(mark);
+        std::cout << "What is your mark? (as %): ";
+        std::cin >> mark;
+    }
+
+    average = smallestNumber(grades);
+    std::cout << "\nThe average of all grades is " << average << ".";
+
+    std::cout << "\n\n\nDone.\n";
+}
